c94beca373f6ed7a3e3f01cfdf55a40731ef337a
anjasferdiansyah <anjasferdiansyah123@gmail.com>
2025-06-01 19:25:01 +0700

    [FEAT] Add Footer Section

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,41 @@
+import Hero from "~/components/Hero";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import Client from "~/components/Client";
+
+import Projects from "~/components/Projects";
+import { CollaborationModel } from "~/components/CollaborationModel";
+import Testimonial from "~/components/Testimonial";
+import CallToAction from "~/components/CallToAction";
+import Article from "~/components/Article";
+
+
+import Services from "~/components/Services";
+import Footer from "~/components/Footer";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Layang Digital Innovation" },
+    {
+      name: "description",
+      content: "Company Profile Website Layang Digital Innovation",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Client />
+      <Services/>
+      <Projects />
+      <CollaborationModel />
+      <Testimonial />
+      <CallToAction />
+      <Article />
+      <Footer/>
+    </>
+  );
 }
